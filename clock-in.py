bce60f5e42bd4149721061644014c8232b333eb2
QuickMain <1013818197@qq.com>
2022-07-08 16:59:10 +0800

    Update clock-in.py
    fix: 尝试修复2022/7/8出现的数据提交失败

--- clock-in.py
+++ clock-in.py
@@ -115,6 +115,7 @@ class ClockIn(object):
         # form change
         new_info['jrdqtlqk[]'] = 0
         new_info['jrdqjcqk[]'] = 0
+        new_info['internship'] = 1  # 是否实习 1:否 2:校内 3:校外
         new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
         new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
         new_info['sfqrxxss'] = 1    # 是否确认信息属实
